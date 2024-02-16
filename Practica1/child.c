62570a17b256b7a6a9fcdf2d00e1ec221d0e7216
ErickMayorgaR <3667150890101@ingenieria.usac.edu.gt>
2024-02-16 16:32:17 -0600

    end
    se terminaron las funcionaldiades

--- Practica1/child.c
+++ Practica1/child.c
@@ -4,11 +4,26 @@
 #include <fcntl.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
+
+
+volatile sig_atomic_t sign = 0;
+int total = 0;
+int c_read = 0;
+int c_write = 0;
+int seek = 0;
 
 char random_char() {
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     const size_t charset_size = sizeof(charset) - 1;
     return charset[rand() % charset_size];
+}
+
+void sigint_handler()
+{
+    printf("\nTerminando proceso hijo...\n");
+    sign = -1;
+    exit(0);
 }
 
 int main() {
@@ -20,32 +35,37 @@ int main() {
         exit(1);
     }
 
-    // Ciclo para simular llamadas de sistema
-    for (int i = 0; i < 10; ++i) {
-        int random_num = rand() % 3; // Generar un número aleatorio entre 0 y 2
+    while (!sign) {
+        int random_num = rand() % 3;
 
-        // Realizar la llamada de sistema correspondiente según el número aleatorio
         switch (random_num) {
             case 0: {
-                char random_string[9]; // Buffer para la cadena aleatoria
+                
+                char random_string[9]; 
                 for (int j = 0; j < 8; ++j) {
                     random_string[j] = random_char(); // Generar un caracter aleatorio
                 }
-                random_string[8] = '\0'; // Terminador de cadena
+                random_string[8] = '\0'; 
                 write(fd, random_string, strlen(random_string)); // Escribir en el archivo
-                printf("Proceso hijo escribió: %s\n", random_string);
+                printf("\n");
+                printf("Proceso hijo escribió: %s", random_string);
+                printf("\n");
                 break;
             }
             case 1: {
-                char read_buffer[9]; // Buffer para la lectura
-                read(fd, read_buffer, 8); // Leer del archivo
-                read_buffer[8] = '\0'; // Terminador de cadena
-                printf("Proceso hijo leyó: %s\n", read_buffer);
+                char read_buffer[9]; 
+                read(fd, read_buffer, 8); 
+                read_buffer[8] = '\0'; 
+                printf("\n");
+                printf("Proceso hijo leyó: %s", read_buffer);
+                printf("\n");
                 break;
             }
             case 2: {
-                lseek(fd, 0, SEEK_SET); // Reposicionar el puntero de lectura/escritura al principio del archivo
-                printf("Proceso hijo reposicionó el puntero\n");
+                lseek(fd, 0, SEEK_SET); 
+                printf("\n");
+                printf("Proceso hijo reposicionó el puntero");
+                printf("\n");
                 break;
             }
         }
@@ -53,8 +73,6 @@ int main() {
         sleep(rand() % 3 + 1); // Esperar entre 1 y 3 segundos
     }
 
-    // Cerrar el archivo
     close(fd);
-
     return 0;
 }
--- Practica1/parent.c
+++ Practica1/parent.c
@@ -4,21 +4,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <string.h>
+
+pid_t pid1, pid2, m_pid;
 
 // Función para manejar la señal SIGINT (Ctrl + C)
-void sigint_handler(int signum)
+void sigint_handler()
 {
     printf("\nTerminando proceso padre...\n");
-    // Aquí puedes imprimir el número total de llamadas al sistema que realizaron los procesos hijo
-    // También puedes imprimir el número de llamadas al sistema por tipo
+    kill(pid1, SIGINT);
+    kill(pid2, SIGINT);
+    kill(m_pid, SIGINT);
     exit(0);
 }
 
 int main()
 {
     signal(SIGINT, sigint_handler);
-
-    pid_t pid1, pid2;
 
     pid1 = fork();
     if (pid1 == 0)
@@ -34,12 +36,64 @@ int main()
             execl("./child.bin", "child.bin", NULL);
             exit(0);
         }
+        else
+        {
+            m_pid = fork();
+            if (m_pid == 0)
+            {
+                char command[100];
+                sprintf(command, "%s %d %d %s", "sudo stap monitor.stp", pid1, pid2, "> syscalls.log");
+                system(command);
+                exit(0);
+            }
+            else
+            {
+                waitpid(pid1, NULL, 0);
+                waitpid(pid2, NULL, 0);
+                waitpid(m_pid, NULL, 0);
+
+                FILE *archivo;
+                char linea[100];
+
+                int total_read = 0;
+                int total_write = 0;
+                int total_seek = 0;
+                int total_llamadas = 0;
+
+                archivo = fopen("./syscalls.log", "r");
+                if (archivo == NULL)
+                {
+                    perror("Error al abrir el archivo");
+                    return 1;
+                }
+
+                while (fgets(linea, sizeof(linea), archivo))
+                {
+                    total_llamadas++;
+
+                    if (strstr(linea, "read"))
+                    {
+                        total_read++;
+                    }
+                    else if (strstr(linea, "write"))
+                    {
+                        total_write++;
+                    }
+                    else if (strstr(linea, "seek"))
+                    {
+                        total_seek++;
+                    }
+                }
+
+                // Imprime el resultado
+                printf("Total de llamadas de read: %d\n", total_read);
+                printf("Total de llamadas de write: %d\n", total_write);
+                printf("Total de llamadas de seek: %d\n", total_seek);
+                printf("Total de llamadas al sistema: %d\n", total_llamadas);
+
+                fclose(archivo);
+            }
+        }
     }
-
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-
-    printf("Terminando proceso padre...\n");
-
     return 0;
 }
